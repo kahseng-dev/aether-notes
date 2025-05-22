29104ea86986bf9bce88d7583262f10fd484d793
kahseng-dev <kahseng.chew@protonmail.com>
2025-05-23 07:48:20 +0800

    feat: add header section

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
 import Image from "next/image";
+import Link from "next/link";
 import { getAllNotes } from "@/lib/notes-data";
-import type { Note } from "@/types/Note"
+import type { Note } from "@/types/Note";
+import HomeDirectory from "@/app/ui/home-directory";
+import { Settings, Search } from 'lucide-react';
 
 export default function Home() {
+  const iconSize = 24;
+
   const data:Note[] = getAllNotes();
 
   let headerTitle = "aether-notes"
@@ -10,20 +15,21 @@ export default function Home() {
   return (
     <div className="p-8 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col">
-        <div className="flex">
-          {headerTitle}
+        <div className="p-2 flex items-center justify-between">
+          <Link
+            href="/settings"
+            className="p-2 rounded transition duration-300 ease-in-out hover:bg-white/20">
+              <Settings size={iconSize}/>
+          </Link>
+          <p>{headerTitle}</p>
+          <Link
+            href="/search"
+            className="p-2 rounded transition duration-300 ease-in-out hover:bg-white/20">
+              <Search size={iconSize}/>
+          </Link>
         </div>
         <div className="flex flex-col">
-          {data.map(item =>
-            <a
-              href={`/editor/${item.id}`}
-              key={item.id}
-              className="flex flex-col">
-              <p>{item.title}</p>
-              <p>{item.date}</p>
-              <p>{item.content}</p>
-            </a>
-          )}
+          <HomeDirectory notes={data}/>
         </div>
       </main>
     </div>
