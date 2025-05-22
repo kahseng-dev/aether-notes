b030a6be435d113d7e1636c02793cf34fe2ef81e
kahseng-dev <kahseng.chew@protonmail.com>
2025-05-22 13:52:50 +0800

    feat: add basic notes data

---
+++ lib/data.ts
@@ -0,0 +1,13 @@
+const data = [
+    {
+        "id": 1,
+        "title": "Walk Around the City",
+        "date": "2023-10-01",
+        "content": ["Took a long walk around the city today. It was a beautiful day and I enjoyed the fresh air. I stopped by a few shops and picked up some groceries. Overall, it was a great day to be outside."],
+        "folder": "Notes",
+    }
+];
+
+export function getData() {
+    return data
+};
