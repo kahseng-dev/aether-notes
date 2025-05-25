47bd564f69053494993745249fc1bc1f20e2574c
kahseng-dev <kahseng.chew@protonmail.com>
2025-05-25 09:13:37 +0800

    feat: add another entry for notes placeholder data

--- lib/notes-data.ts
+++ lib/notes-data.ts
@@ -2,18 +2,31 @@ import type { Note } from "@/types/Note"
 
 const data:Note[] = [
     {
-        "id": 1,
-        "title": "Walk Around the City",
-        "date": "2023-10-01",
-        "content": ["Took a long walk around the city today. It was a beautiful day and I enjoyed the fresh air. I stopped by a few shops and picked up some groceries. Overall, it was a great day to be outside."],
-        "folder": "Notes",
-    }
+        id: 1,
+        title: "Walk Around the City",
+        lastUpdated: "2023-05-10",
+        content: [
+            "Took a long walk around the city today. It was a beautiful day and I enjoyed the fresh air. I stopped by a few shops and picked up some groceries. Overall, it was a great day to be outside."
+        ],
+        folder: "Notes",
+    },
+    {
+        id: 2,
+        title: "Shopping List",
+        lastUpdated: "2023-05-24",
+        content: [
+            "[ ] Apple",
+            "[ ] Milk",
+            "[ ] Eggs",
+        ],
+        folder: "Todos",
+    },
 ];
 
 export function getAllNotes() {
     return data as Note[]
-}
+};
 
-export function getNote(id:number) {
-    return data[id] as Note
+export function getNote(id: number) {
+    return data.find(note => note.id === id) as Note;
 };
